6cb76516d83385e2e1717e270b93c0091cd753b3
Francesco Garosi <francesco.garosi@gmail.com>
2025-01-18 19:43:19 +0100

    Bump version tag
    Bump version to 1.9.0-beta: the application is quite stable and runs both on Linux and on Windows. It might require some adjustments and enhancements though, and from now on the same effort will be put both in spotting bugs and optimizations and in enhancements, especially concerning new types of item that will be configured in a simple way -- as it used to happen in the old (unsupported) version.

--- lib/i18n/strings_base.py
+++ lib/i18n/strings_base.py
@@ -4,7 +4,7 @@
 UI_APP = "When"
 UI_APP_LABEL = "When Automation Tool"
 UI_APP_COPYRIGHT = "© 2023-2025 Francesco Garosi"
-UI_APP_VERSION = "1.9.0-alpha"
+UI_APP_VERSION = "1.9.0-beta"
 
 
 # item types
