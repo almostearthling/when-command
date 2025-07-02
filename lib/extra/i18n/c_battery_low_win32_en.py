377006c8898e6fa5f2a8ff3c55b8caa687826390
Francesco Garosi <francesco.garosi@gmail.com>
2025-07-02 17:19:09 +0200

    Fix en module for low battery condition

--- lib/extra/i18n/c_battery_low_win32_en.py
+++ lib/extra/i18n/c_battery_low_win32_en.py
@@ -2,7 +2,7 @@
 
 from ...i18n.strings import *
 
-ITEM_COND_LOWBATT = "Low Battery Condition"
+ITEM_HR_NAME = "Low Battery Condition"
 
 UI_FORM_TITLE = f"{UI_APP}: Low Battery Condition Editor"
 UI_FORM_THRESHOLD_SC = "Battery charge is below:"
