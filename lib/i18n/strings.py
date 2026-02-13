ecbc105cf3b80b60a88c1c8816679e8bbd4cd980
Francesco Garosi <francesco.garosi@gmail.com>
2026-02-13 11:25:03 +0100

    docs: update patchlevel in string resources
    Also, minor documentation updates

--- lib/i18n/strings.py
+++ lib/i18n/strings.py
@@ -15,7 +15,7 @@ UI_APP = "When"
 CLI_APP = "when"
 UI_APP_LABEL = "When Automation Tool"
 UI_APP_COPYRIGHT = "© 2023-2026 Francesco Garosi"
-UI_APP_VERSION = "2.0.0"
+UI_APP_VERSION = "2.0.1"
 
 # other strings that should not be translated
 UI_WHENEVER = "Whenever"
