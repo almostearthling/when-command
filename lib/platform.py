73bc4c750f8dcb922d249e935cba42b2d7a6a75d
Francesco Garosi <francesco.garosi@gmail.com>
2026-06-12 23:36:34 +0200

    style: reformat file

--- lib/platform.py
+++ lib/platform.py
@@ -4,12 +4,10 @@ import sys
 import os
 import shutil
 
-
 _PLATFORM = None
 
 _COMMANDS_AVAILABLE = []
 _COMMANDS_UNAVAILABLE = []
-
 
 
 def is_linux() -> bool:
@@ -24,6 +22,7 @@ def is_linux() -> bool:
     else:
         return bool(_PLATFORM == "linux")
 
+
 def is_windows() -> bool:
     global _PLATFORM
     if _PLATFORM is None:
@@ -36,6 +35,7 @@ def is_windows() -> bool:
     else:
         return bool(_PLATFORM == "windows")
 
+
 def is_mac() -> bool:
     global _PLATFORM
     if _PLATFORM is None:
@@ -47,6 +47,7 @@ def is_mac() -> bool:
             return False
     else:
         return bool(_PLATFORM == "mac")
+
 
 def has_command(cmd: str) -> bool:
     global _COMMANDS_AVAILABLE, _COMMANDS_UNAVAILABLE
@@ -62,6 +63,7 @@ def has_command(cmd: str) -> bool:
             _COMMANDS_AVAILABLE.append(cmd)
             return True
 
+
 def is_command(path):
     if os.path.exists(path) and os.access(path, os.X_OK):
         return True
@@ -69,6 +71,7 @@ def is_command(path):
         return True
     else:
         return False
+
 
 def is_dir(path):
     if os.path.exists(os.path.join(path, ".")):
